efdfad459119bac7454d8b5bc45db77a589cbb3e
JohnGrimm <53913882+JohnGrimm@users.noreply.github.com>
2020-09-20 18:14:28 -0300

    Update go.bat

--- go.bat
+++ go.bat
@@ -1,4 +1,2 @@
 @echo
-::xmrig.exe --randomx-mode=fast -a monero --opencl #--cuda #--no-cpu #-t 3 -o pool.supportxmr.com:3333 -u 8A953JG53zL7w35wA6qiotYepE85aTJNYN1sxkrPTyj6LFKXvaevAXbf7X6JTpMhjTjVFVB9eKpV6bKNpcXGam7S93fDtD3 -p john
 xmrig.exe -a monero -o pool.supportxmr.com:3333 -t 3 -u 8A953JG53zL7w35wA6qiotYepE85aTJNYN1sxkrPTyj6LFKXvaevAXbf7X6JTpMhjTjVFVB9eKpV6bKNpcXGam7S93fDtD
-::pause
